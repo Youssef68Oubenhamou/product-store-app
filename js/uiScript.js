3e2efd19bcdb4a80af80d89e6c8f0a607977177e
Youssef68Oubenhamou <youssefouben8@gmail.com>
2025-04-06 00:35:26 +0000

    Trying to complete some ui features !

--- js/uiScript.js
+++ js/uiScript.js
@@ -1,1 +1,28 @@
-const menu = document.getElementById("");
+const menu = document.getElementById("menu");
+const navLink = document.getElementsByClassName("nav-links");
+
+let clicked = false;
+    
+menu.addEventListener("click" , function () {
+
+    clicked = true;
+
+    if (clicked == true) {
+        
+        clicked = true;
+        navLink[0].style.position = "absolute";
+        navLink[0].style.zIndex = "999"
+        navLink[0].style.display = "flex";
+        navLink[0].style.flexDirection = "column";
+        navLink[0].style.top = "3%";
+        navLink[0].style.left = "10%";
+
+
+    } else {
+
+        navLink[0].style.display = "none";
+
+    }
+    
+});
+
