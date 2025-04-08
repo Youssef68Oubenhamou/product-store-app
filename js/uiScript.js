4e36375d92fa2d75faad546281a85e004ee3dea1
Youssef68Oubenhamou <youssefouben8@gmail.com>
2025-04-08 21:47:35 +0100

    Working on the feature of navigation bar !

--- js/uiScript.js
+++ js/uiScript.js
@@ -5,22 +5,20 @@ let clicked = false;
     
 menu.addEventListener("click" , function () {
 
-    clicked = true;
-
-    if (clicked == true) {
+    if (clicked == false) {
         
-        clicked = true;
         navLink[0].style.position = "absolute";
         navLink[0].style.zIndex = "999"
         navLink[0].style.display = "flex";
         navLink[0].style.flexDirection = "column";
         navLink[0].style.top = "3%";
         navLink[0].style.left = "10%";
-
+        clicked = true;
 
     } else {
 
         navLink[0].style.display = "none";
+        clicked = false;
 
     }
     
